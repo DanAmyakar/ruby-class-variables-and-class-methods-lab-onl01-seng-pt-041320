7015901943c75c642ca311cef1e9a37f976b423b
Daniel Amyakar <dan.rockit@gmail.com>
2020-04-28 19:57:57 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,4 +1,10 @@
 class Song
-  @@song_count = 0
-  @@song_artists
+  @@count = 0
+  @@artists = []
+  @@genres = []
+  def initialize
+    @name = name
+    @@count++
+    @@genre << genre
+    @@artists << artists
 end
